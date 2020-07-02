2d9313bd31608c2e4bc91d40744c61399a66122e
Dane M Ogden <DaneOgden1@gmail.com>
2020-07-02 16:09:56 -0500

    jsx conditionals ternary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-function App() {
-	return (
-		<div className='App'>
-			<h1>GITHUB APP</h1>
-		</div>
-	);
+class App extends Component {
+	//class method:
+	foobar = () => 'FOOBAR';
+
+	render() {
+		const name = 'somebody famous';
+
+		const loading = false;
+		const loaded = !loading;
+
+		// NOTE use conditionals above the return of render
+		if (loading) {
+			return <h3>Loading ...</h3>;
+		}
+
+		const foo = () => 'Bar ';
+		return (
+			<div className='App'>
+				<h1>GITHUB APP </h1>
+				{loaded ? (
+					<h4>Loading...</h4>
+				) : (
+					<React.Fragment>
+						<h2>Hey {name.toUpperCase()}</h2>
+						<h3> method of render: {foo()}</h3>
+						<h3>method of class: {this.foobar()}</h3>
+						<h3>Conditional:</h3>
+					</React.Fragment>
+				)}
+			</div>
+		);
+	}
 }
 
 export default App;
