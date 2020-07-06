99c586b3a6b9ccf0361e22a38c2ff19d0547e953
Dane M Ogden <DaneOgden1@gmail.com>
2020-07-06 12:52:38 -0500

    add alert for empty search entry and null state wihtin method timeout
    onSubmit -> if text state empyt setAlert prop object.
    git commit -m

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ class App extends Component {
 	state = {
 		users: [],
 		loading: false,
+		alert: null,
 	};
 
 	// NOTE: in class App extends Component {
@@ -41,6 +43,13 @@ class App extends Component {
 	//Clear users from state
 	clearUsers = () => this.setState({ users: [], loading: false });
 
+	//setAlert
+	setAlert = (msg, type) => {
+		this.setState({ alert: { msg: msg, type: type } });
+
+		setTimeout(() => this.setState({ alert: null }), 3000);
+	};
+
 	render() {
 		const { users, loading } = this.state;
 		return (
@@ -50,10 +59,12 @@ class App extends Component {
 					<Navbar />
 				</nav>
 				<div className='container'>
+					<Alert alert={this.state.alert} />
 					<Search
 						searchUsers={this.searchUsers}
 						clearUsers={this.clearUsers}
 						showClear={users.length > 0 ? true : false}
+						setAlert={this.setAlert}
 					/>
 					<Users loading={loading} users={users} />
 				</div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,14 +8,20 @@ export class Search extends Component {
 	};
 
 	static propTypes = {
-		searcUsers: PropTypes.func.isRequired,
+		searchUsers: PropTypes.func.isRequired,
 		clearUsers: PropTypes.func.isRequired,
 		showClear: PropTypes.bool.isRequired,
+		setAlert: PropTypes.func.isRequired,
 	};
+
 	onSubmit = (e) => {
 		e.preventDefault();
-		this.props.searchUsers(this.state.text);
-		this.setState({ text: '' });
+		if (this.state.text === '') {
+			this.props.setAlert('Please enter something', 'light');
+		} else {
+			this.props.searchUsers(this.state.text);
+			this.setState({ text: '' });
+		}
 	};
 	onChange = (event) => {
 		this.setState({ [event.target.name]: event.target.value });
