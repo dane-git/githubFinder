5fef181dc9e0988127b3edf766856a8aaa28911b
Dane M Ogden <DaneOgden1@gmail.com>
2020-07-07 07:40:00 -0500

    add Repos and RepoItem to display 5 most recent of user repos
    getUserRepos() in App.js -> API call with paging
    Repos.js -> map array of repos to ReposItem
    RepoItem.js -> single repos display

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
 	state = {
 		users: [],
 		user: {},
+		repos: [],
 		loading: false,
 		alert: null,
 	};
@@ -54,6 +55,16 @@ class App extends Component {
 		this.setState({ loading: false, user: res.data });
 	};
 
+	// Get user repos
+	getUserRepos = async (username) => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client)secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({ loading: false, repos: res.data });
+	};
+
 	//Clear users from state
 	clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -65,7 +76,7 @@ class App extends Component {
 	};
 
 	render() {
-		const { users, loading, user } = this.state;
+		const { users, loading, user, repos } = this.state;
 
 		return (
 			<Router>
@@ -100,7 +111,9 @@ class App extends Component {
 									<User
 										{...props}
 										getUser={this.getUser}
+										getUserRepos={this.getUserRepos}
 										user={user}
+										repos={repos}
 										loading={loading}
 									/>
 								)}
